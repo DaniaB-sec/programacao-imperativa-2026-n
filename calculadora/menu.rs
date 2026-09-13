ec9e9fbe245e12885b633d4f88783601ca0699cf
Danya <daniabeck.sec@gmail.com>
2026-09-13 15:36:23 -0300

    Add files via upload

---
+++ calculadora/menu.rs
@@ -0,0 +1,12 @@
+
+pub fn menu1(){
+    use std::io;
+        println!("Escolha uma operação:");
+        println!("1. Soma");
+        println!("2. Subtração");
+        println!("3. Divisão");
+        println!("4. Multiplicação");
+        println!("5. Sair");
+    
+    
+}
---
+++ calculadora/operacoes.rs
@@ -0,0 +1,26 @@
+
+
+    
+    pub fn adi(a:i32,b:i32){
+        let mut resultado = 0;
+        resultado = a + b;
+        println!("Resultado: {}", resultado)
+    }
+    pub fn sub(a:i32,b:i32){
+        let mut resultado = 0;
+        resultado = a - b;
+        println!("Resultado: {}", resultado)
+    }
+
+    pub fn multi(a:i32,b:i32){
+        let mut resultado = 0;
+        resultado = a * b;
+        println!("Resultado: {}", resultado)
+    }
+
+    pub fn div(a:i32,b:i32){
+        let mut resultado = 0;
+        resultado = a + b;
+        println!("Resultado: {}", resultado)
+    }
+     
